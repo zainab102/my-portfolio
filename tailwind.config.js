ca4cabdd71d7bd35bba83d2bf73c3bc83f23faae
Zainab <tariqzainab1190@gmail.com>
2025-08-05 12:28:54 +0500

    Fix Tailwind config path case sensitivity

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
