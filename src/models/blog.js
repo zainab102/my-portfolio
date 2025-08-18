fe99aca23b541f2884335b0543b7fded5b1b4340
Zainab <tariqzainab1190@gmail.com>
2025-08-18 08:35:22 +0500

    Update page.js and components: remove duplicate footer, add 'View More' links

--- src/models/blog.js
+++ src/models/blog.js
@@ -1,41 +1,49 @@
 import clientPromise from "@/lib/mongodb";
+import { ObjectId } from "mongodb";
 
-const dbName = "myPortfolioDB"; // choose your database name
-const collectionName = "blogs";
-
-export async function getCollection() {
-  const client = await clientPromise;
-  const db = client.db(dbName);
-  return db.collection(collectionName);
-}
-
-export async function getAllBlogs() {
-  const collection = await getCollection();
-  return collection.find({}).sort({ _id: -1 }).toArray();
-}
-
-export async function getBlogBySlug(slug) {
-  const collection = await getCollection();
-  return collection.findOne({ slug });
-}
+const DB_NAME = "myPortfolioDB"; // Change if needed
+const COLLECTION_NAME = "blogs";
 
 export async function addBlog(blog) {
-  const collection = await getCollection();
-  const result = await collection.insertOne(blog);
-  return result;
+  const client = await clientPromise;
+  const db = client.db(DB_NAME);
+  const collection = db.collection(COLLECTION_NAME);
+  return await collection.insertOne(blog);
 }
 
 export async function updateBlog(id, updatedBlog) {
-  const collection = await getCollection();
-  const result = await collection.updateOne(
-    { _id: new ObjectId(id) },
-    { $set: updatedBlog }
-  );
-  return result;
+  const client = await clientPromise;
+  const db = client.db(DB_NAME);
+  const collection = db.collection(COLLECTION_NAME);
+  return await collection.updateOne({ _id: new ObjectId(id) }, { $set: updatedBlog });
 }
 
 export async function deleteBlog(id) {
-  const collection = await getCollection();
-  const result = await collection.deleteOne({ _id: new ObjectId(id) });
-  return result;
+  const client = await clientPromise;
+  const db = client.db(DB_NAME);
+  const collection = db.collection(COLLECTION_NAME);
+  return await collection.deleteOne({ _id: new ObjectId(id) });
+}
+
+export async function getBlogs(page = 1, limit = 10) {
+  const client = await clientPromise;
+  const db = client.db(DB_NAME);
+  const collection = db.collection(COLLECTION_NAME);
+
+  const blogs = await collection
+    .find({})
+    .sort({ createdAt: -1 })
+    .skip((page - 1) * limit)
+    .limit(limit)
+    .toArray();
+
+  return blogs;
+}
+
+export async function getBlogBySlug(slug) {
+  const client = await clientPromise;
+  const db = client.db(DB_NAME);
+  const collection = db.collection(COLLECTION_NAME);
+
+  return await collection.findOne({ slug });
 }
